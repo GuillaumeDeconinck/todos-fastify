2909c51bde0f5dd768cee8fa4c8d0bb0345350e5
Guillaume Deconinck <gui.deconinck@gmail.com>
2021-07-26 16:22:09 +0200

    test(e2e): slightly improve e2e tests

--- __tests__/e2e/api/todos/todos.test.ts
+++ __tests__/e2e/api/todos/todos.test.ts
@@ -1,9 +1,22 @@
 import got from "got";
+import { Todo } from "../../../../src/domain/models/Todo";
 import { API_URL } from "../../helpers/environment";
 
 describe("Testing the todos API", () => {
   it("should return a 200 when listing todos", async () => {
-    const response = await got<unknown[]>(`${API_URL}/v1/todos`, {
+    const response = await got<Todo[]>(`${API_URL}/v1/todos`, {
+      method: "GET",
+      responseType: "json",
+      retry: 0,
+      throwHttpErrors: false
+    });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toEqual([]);
+  });
+
+  it("should return a 200 when retrieving a single todo", async () => {
+    const response = await got<Todo[]>(`${API_URL}/v1/todos/shouldBeAUuidAtSomePoint`, {
       method: "GET",
       responseType: "json",
       retry: 0,
