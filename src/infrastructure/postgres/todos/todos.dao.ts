c9700be0fec305a43ebaa5e5e7dcd18ebbea05de
Guillaume Deconinck <gui.deconinck@gmail.com>
2021-07-27 10:56:23 +0200

    feat: implement base dao for todos

--- src/infrastructure/postgres/todos/todos.dao.ts
+++ src/infrastructure/postgres/todos/todos.dao.ts
@@ -1,16 +1,32 @@
-import { registry, singleton } from "tsyringe";
+import { delay, inject, injectable, registry, singleton } from "tsyringe";
+import { PostgresPool } from "..";
 import { Todo } from "../../../domain/models/Todo";
 import { TodosRepository } from "../../../domain/repositories/todos.repository";
+import { todosQueries } from "./queries";
 
 // As interfaces do not exist in JS
 // We need to use a string as the token to specify the wanted implem
 @registry([{ token: "TodosRepository", useClass: TodosDaoService }])
 @singleton()
+@injectable()
 export class TodosDaoService implements TodosRepository {
+  constructor(@inject(delay(() => PostgresPool)) private pool: PostgresPool) {}
+
   async listTodos(ownerUuid: string): Promise<Todo[]> {
-    return [];
+    return await this.pool.executeQuery<Todo>("listTodos", todosQueries.listTodos, [ownerUuid]);
   }
+
   async getTodo(ownerUuid: string, todoUuid: string): Promise<Todo> {
-    return undefined;
+    const result = await this.pool.executeQuery<Todo>("getTodo", todosQueries.getTodo, [todoUuid, ownerUuid]);
+
+    if (result.length === 0) {
+      throw new Error("Todo not found");
+    }
+
+    if (result.length > 1) {
+      throw new Error("Found more than 1 todos");
+    }
+
+    return result[0];
   }
 }
