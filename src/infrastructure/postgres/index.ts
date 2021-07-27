002e2785daf6eab385f539b941b90c8bf9d1866f
Guillaume Deconinck <gui.deconinck@gmail.com>
2021-07-27 10:55:52 +0200

    featL add postgres's pool class

--- src/infrastructure/postgres/index.ts
+++ src/infrastructure/postgres/index.ts
@@ -1,4 +1,53 @@
+import * as postgres from "pg";
+import { inject, injectable, singleton } from "tsyringe";
+import { AppConfiguration } from "../../tools/config";
+import { Logger } from "../../tools/logger";
 // Import the DAOs in order for the @registry to be correctly taken into account
 import "./todos/todos.dao";
 
 // To be implemented, base class for Postgres
+@singleton()
+@injectable()
+export class PostgresPool {
+  private pool: postgres.Pool;
+
+  constructor(
+    @inject(AppConfiguration) private configuration: AppConfiguration,
+    @inject(Logger) private logger: Logger
+  ) {}
+
+  async connect(): Promise<void> {
+    this.pool = new postgres.Pool({
+      ...this.configuration.getPGConfig(),
+      min: 1,
+      max: 5
+    });
+
+    // Check if pool works fine
+    const client = await this.pool.connect();
+    await client.query("SELECT NOW()");
+    client.release();
+  }
+
+  async getClient(): Promise<postgres.PoolClient> {
+    return await this.pool.connect();
+  }
+
+  async executeQuery<T>(queryName: string, query: string, params: unknown[]): Promise<T[]> {
+    try {
+      const client = await this.getClient();
+      const result = await client.query(query, params);
+      return result.rows;
+    } catch (error) {
+      this.logger.logger.error(`[PGSQL] Error while executing ${queryName}`);
+      this.logger.logger.error(error);
+      throw error;
+    }
+  }
+
+  async close(): Promise<void> {
+    if (this.pool) {
+      this.pool.end();
+    }
+  }
+}
